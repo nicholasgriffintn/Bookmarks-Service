a23bea7c8d9ff1c41aead7ec1436f30a2837e0d0
Nicholas Griffin <me@nicholasgriffin.co.uk>
2021-08-30 13:23:30 +0100

    WIP: Few changes to the code to get it to run
    Commenting out GQL stuff for now

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ const { config } = require('./config');
 const s3 = new AWS.S3();
 const dynamodb = new AWS.DynamoDB();
 
-const { graphqlLambda } = require('apollo-server-lambda');
+/* const { graphqlLambda } = require('apollo-server-lambda');
 const { makeExecutableSchema } = require('graphql-tools');
 const { schema } = require('./schema');
 const { resolvers } = require('./resolvers');
@@ -12,7 +12,7 @@ const { resolvers } = require('./resolvers');
 const graphQLSchema = makeExecutableSchema({
   typeDefs: schema,
   resolvers,
-});
+}); */
 
 module.exports.process = async (event) => {
   if (event.Records && event.Records[0] && event.Records[0].s3) {
@@ -43,112 +43,122 @@ module.exports.process = async (event) => {
 
       if (data) {
         console.info('parsing the bookmark...');
-        console.log(data);
 
-        const { id, recieved, subject, from, to, html } = data;
+        var fileContents = data.Body.toString();
+        var json = JSON.parse(fileContents);
 
-        const fromAddress = from.value[0].address;
-        const fromName = from.value[0].name;
+        if (json && json.id) {
+          const { id, recieved, subject, from, to, html } = json;
 
-        const toAddress = to.value[0].address;
-        const toName = to.value[0].name;
+          const fromAddress = from.value[0].address;
+          const fromName = from.value[0].name;
 
-        // TODO: parse the html for bookmark data
-        const bookmark = {};
+          const toAddress = to.value[0].address;
+          const toName = to.value[0].name;
 
-        function extractData(data, startStr, endStr) {
-          subStrStart = data.indexOf(startStr) + startStr.length;
-          return data.substring(
-            subStrStart,
-            subStrStart + data.substring(subStrStart).indexOf(endStr)
+          // TODO: parse the html for bookmark data
+          const bookmark = {};
+
+          function extractData(data, startStr, endStr) {
+            subStrStart = data.indexOf(startStr) + startStr.length;
+            return data.substring(
+              subStrStart,
+              subStrStart + data.substring(subStrStart).indexOf(endStr)
+            );
+          }
+
+          bookmark.title = extractData(html, '<div>Title: ', '</div>');
+          bookmark.description = extractData(
+            body,
+            '<div>Description: ',
+            '</div>'
           );
-        }
+          bookmark.url = extractData(body, '<div>URL: ', '</div>');
 
-        bookmark.title = extractData(html, 'Title: ', '\n');
-        bookmark.description = extractData(body, 'Description: ', '\n');
-        bookmark.url = extractData(body, 'URL: ', '\n');
+          console.log(bookmark);
 
-        console.log(bookmark);
+          if (
+            bookmark &&
+            bookmark.title &&
+            bookmark.description &&
+            bookmark.url
+          ) {
+            console.info('Storing parsed bookmark...');
 
-        if (
-          bookmark &&
-          bookmark.title &&
-          bookmark.description &&
-          bookmark.url
-        ) {
-          console.info('Storing parsed bookmark...');
+            const dataOutput = {
+              Item: {
+                id: {
+                  S: id,
+                },
+                status: {
+                  S: 'unverified',
+                },
+                recieved: {
+                  S: recieved,
+                },
+                bookmark: {
+                  S: JSON.stringify(bookmark),
+                },
+                subject: {
+                  S: subject,
+                },
+                fromAddress: {
+                  S: fromAddress,
+                },
+                fromName: {
+                  S: fromName,
+                },
+                toAddress: {
+                  S: toAddress,
+                },
+                toName: {
+                  S: toName,
+                },
+              },
+            };
 
-          const dataOutput = {
-            Item: {
-              id: {
-                S: id,
-              },
-              status: {
-                S: 'unverified',
-              },
-              recieved: {
-                S: recieved,
-              },
-              bookmark: {
-                S: JSON.stringify(bookmark),
-              },
-              subject: {
-                S: subject,
-              },
-              fromAddress: {
-                S: fromAddress,
-              },
-              fromName: {
-                S: fromName,
-              },
-              toAddress: {
-                S: toAddress,
-              },
-              toName: {
-                S: toName,
-              },
-            },
-          };
+            var params = {
+              Item: dataOutput.Item,
+              TableName: config.tableName,
+            };
 
-          var params = {
-            Item: dataOutput.Item,
-            TableName: config.tableName,
-          };
+            dynamodb.putItem(params, async function (err, data) {
+              if (err) {
+                throw new Error(err);
+              } else {
+                console.log(data);
+                await s3
+                  .deleteObject({
+                    Bucket: bookmarkBucket,
+                    Key: bookmarkKey,
+                  })
+                  .promise();
 
-          dynamodb.putItem(params, async function (err, data) {
-            if (err) {
-              throw new Error(err);
-            } else {
-              console.log(data);
-              await s3
-                .deleteObject({
-                  Bucket: bookmarkBucket,
-                  Key: bookmarkKey,
-                })
-                .promise();
+                console.log(dataOutput);
 
-              console.log(dataOutput);
+                return {
+                  statusCode: 200,
+                  body: JSON.stringify({
+                    message: dataOutput,
+                    event,
+                  }),
+                };
+              }
+            });
+          } else {
+            await s3
+              .deleteObject({
+                Bucket: bookmarkBucket,
+                Key: bookmarkKey,
+              })
+              .promise();
 
-              return {
-                statusCode: 200,
-                body: JSON.stringify({
-                  message: dataOutput,
-                  event,
-                }),
-              };
-            }
-          });
+            throw new Error(
+              'Data for bookmark could not be found, this email has been deleted.'
+            );
+          }
         } else {
-          await s3
-            .deleteObject({
-              Bucket: bookmarkBucket,
-              Key: bookmarkKey,
-            })
-            .promise();
-
-          throw new Error(
-            'Data for bookmark could not be found, this email has been deleted.'
-          );
+          throw new Error('file did not contain expected data.');
         }
       } else {
         throw new Error('No data found.');
@@ -163,12 +173,13 @@ module.exports.process = async (event) => {
 };
 
 module.exports.graphql = async (event, context, callback) => {
-  function callbackWithHeaders(error, output) {
+  /* function callbackWithHeaders(error, output) {
     // eslint-disable-next-line no-param-reassign
     output.headers['Access-Control-Allow-Origin'] = '*';
     callback(error, output);
   }
 
   const handler = graphqlLambda({ schema: graphQLSchema });
-  return handler(event, context, callbackWithHeaders);
+  return handler(event, context, callbackWithHeaders); */
+  throw new Error('Internal server error');
 };
